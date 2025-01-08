86bfc1b41d07ee3d0f2a6377172c17e3943d4b4b
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-01-08 17:02:35 +0530

    switch

--- day2/index.js
+++ day2/index.js
@@ -3,15 +3,34 @@ const port = 8080;
 const fs = require("fs");
 http
   .createServer((req, res) => {
-    fs.readFile("node.txt", "utf8", function (err, data) {
-      // Display the file content
+    console.log(req.url);
+    let filename = "";
 
-      res.write(`<h1>hello from server ${data}</h1>`);
-      res.end();
+    switch (req.url) {
+      case "/":
+        filename = "index.html";
+
+        break;
+      case "/about":
+        filename = "about.html";
+        break;
+      default:
+        break;
+    }
+    fs.readFile(filename, (err, data) => {
+      if (!err) {
+        res.end(data);
+      }
     });
 
-    
+    // fs.readFile("node.txt", "utf8", function (err, data) {
+    //   // Display the file content
+
+    //   // res.write(`<h1>hello from server ${data}</h1>`);
+    //   res.end();
+    // });
   })
+
   .listen(port, (error) => {
     if (error) {
       console.log(error);
