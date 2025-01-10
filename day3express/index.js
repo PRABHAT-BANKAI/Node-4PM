798803682dcb595b3aa362b3a4dc0856e2df4ace
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-01-10 11:19:48 +0530

    express

--- day3express/index.js
+++ day3express/index.js
@@ -1,12 +1,22 @@
 const express = require("express");
 const port = 8081;
 const app = express();
+const path = require("path");
 
 app.set("view engine", "ejs");
+app.use("/assets", express.static(path.join(__dirname, "./assets")));
+
 app.get("/", (req, res) => {
-  res.render("home");
+  return res.render("home");
   // res.write("<h1>hello from server</h1>");
   // res.end();
+});
+
+app.get("/about", (req, res) => {
+  return res.render("about");
+});
+app.get("/services", (req, res) => {
+  res.render("services");
 });
 
 app.listen(port, (error) => {
