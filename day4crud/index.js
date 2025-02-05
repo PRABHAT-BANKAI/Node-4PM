3bd3484177099fbee4785c812ad8431758291a89
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-02-05 15:36:43 +0530

    fix

---
+++ day4crud/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const port = 8081;
+const app = express();
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.use("/assets", express.static(path.join(__dirname, "./assets")));
+
+let data = { name: "batman", city: "gotham", power: "money" };
+
+app.get("/", (req, res) => {
+  return res.render("home",{data});
+  // res.write("<h1>hello from server</h1>");
+  // res.end();
+});
+
+app.get("/about", (req, res) => {
+  return res.render("about");
+});
+app.get("/services", (req, res) => {
+  res.render("services");
+});
+
+app.listen(port, (error) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  console.log(`Server is running on port ${port}`);
+});
---
+++ day4crud/readme.md
@@ -0,0 +1,3 @@
+expressJS is a framework of node.js that create REST full APIs
+folder Structure
+MVC(models views controllers)
