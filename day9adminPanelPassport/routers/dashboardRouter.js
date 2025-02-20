2b8b3bb76e3f28b90a31d70c26991d49a603d251
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-02-20 16:14:27 +0530

    nodemailer

--- day9adminPanelPassport/routers/dashboardRouter.js
+++ day9adminPanelPassport/routers/dashboardRouter.js
@@ -2,6 +2,7 @@ const express = require("express");
 const UserModel = require("../models/UserModel");
 const dashboardRouter = express.Router();
 const passport = require("../middleware/passportLocal");
+const nodemailer = require("nodemailer");
 
 dashboardRouter.get("/dashboard", passport.auth, (req, res) => {
   res.render("dashboard");
@@ -64,5 +65,43 @@ dashboardRouter.post(
     res.redirect("/dashboard");
   }
 );
+
+dashboardRouter.post("/otpCheck",async (req, res) => {
+let getData = await UserModel.findOne({email:req.body.otpEmail});
+if(getData){
+  let otp = Math.floor(Math.random() * 10000);
+  res.cookie("storeOtp",otp);
+  var transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "prabhssgg@gmail.com",
+      pass: "jkyn vite uqau jlmv",
+    },
+  });
+
+  var mailOptions = {
+    from: "prabhssgg@gmail.com",
+    to: req.body.otpEmail,
+    subject: "OTP",
+    text: `OTP ${otp}`,
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+    }
+    res.render("otpPage");
+  });
+}else{
+  console.log("user not found in database")
+  res.redirect("/");
+}
+
+ 
+
+
+});
 
 module.exports = dashboardRouter;
